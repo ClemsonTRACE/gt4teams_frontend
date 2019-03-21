59fce9f167bd05870bf0c246c5153dd37ee268d5
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2019-03-21 12:38:57 -0700

    initial commit of the global navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import M from 'materialize-css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    M.AutoInit();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="row" style={{ display: "flex" }}>
+          <div className="col s11 m10 l11" style={{ flex: 1 }}>
+            <Route path="/pd" exact component={ () => <div className="row"><h2 className="center-align">Iterative Prisoners Dilemma</h2></div> } />
+            <Route path="/bos/" component={ () => <div className="row"><h2 className="center-align">Battle of the Sexes</h2></div> } />
+            <Route path="/hawkdove/" component={ () => <div className="row"><h2 className="center-align">Hawk Dove</h2></div> } />
+            <Route path="/ultimatum/" component={ () => <div className="row"><h2 className="center-align">Ultimatum</h2></div> } />
+            <Route path="/centipede/" component={ () => <div className="row"><h2 className="center-align">Centipede</h2></div> } />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
