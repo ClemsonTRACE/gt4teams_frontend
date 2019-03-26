999391f1f9683c2664946f5422fb5c84aa1cc897
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2019-03-25 23:58:12 -0400

    cumulative state gets rendered and is kept track of

--- src/BattleOfTheSexes.js
+++ src/BattleOfTheSexes.js
@@ -7,31 +7,43 @@ class BoS extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			"history": {
-				0: [0, 0],
-				1: [0, 0],
-				2: [0, 0]
-			}
+			"status": false, 
+			"numEpochs": 2,
+			"numTurns": 2,
+			"gameState": {},
+			"payoffs": {},
+			"epoch": 0,
+			"turn": 0
 		}
+
+		//generating the state dynamically
+		for (let x = 0; x <= this.state.numEpochs; x++) { 
+			this.state["gameState"][x] = {}
+			this.state["payoffs"][x] = {}
+			for (let y = 0; y <= this.state.numTurns; y++) { 
+				console.log(this.state["gameState"])
+				this.state["gameState"][x][y] = [0, 0]
+				this.state["payoffs"][x][y] = [0, 0]
+			}	
+		}
+
 	}
 
 	componentDidMount() {
+		console.log("mounted")
 		M.AutoInit();
 	}
 
-	nice() {
-		alert("hello")
-		let ob = {
-			"gameState": {
-				"0": [2, 1], "1": [2, 1], "2": [2, 1]
-			},
-			"move": 1
-		}
+	nice(move) {
+		let ob = this.state
+		ob["move"] = move
 		let newOb = JSON.stringify(ob)
-		// console.log(newOb, typeof(newOb))
-		axios.post("http://localhost:8000/games/twoByTwo/pd/ppo", ob)
+
+		let self = this		
+		axios.post("http://localhost:8000/games/twoByTwo/bos/ppo", ob)
 		.then(function (response) {
-			console.log(response);
+			console.log(response["data"])
+			self.setState(response["data"])
 		})
 		.catch(function (error) {
 			console.log(error);
@@ -39,41 +51,48 @@ class BoS extends Component {
 	}
 
 	render() {
-		return(
-			<div>
-				<div className="row">
-					<table>
-						<tbody>
-							<tr>
-								<td></td>
-								<td>They go to the Opera</td>
-								<td>They go to the Game</td>
-							</tr>
-							<tr>
-								<td>You go to the Opera</td>
-								<td>3, 2</td>
-								<td>1, 1</td>
-							</tr>
-							<tr>
-								<td>You go to the Game</td>
-								<td>0, 0</td>
-								<td>2, 3</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div className="row">
-					<div className="col l4"></div>
-					<div className="col l2">
-						<button className='btn' type="submit" name="move" value="opera">Opera</button>
+		if (this.state.status == false) {
+			return(
+				<div>
+					<div className="row">
+						<table>
+							<tbody>
+								<tr>
+									<td></td>
+									<td>They go to the Opera</td>
+									<td>They go to the Game</td>
+								</tr>
+								<tr>
+									<td>You go to the Opera</td>
+									<td>3, 2</td>
+									<td>1, 1</td>
+								</tr>
+								<tr>
+									<td>You go to the Game</td>
+									<td>0, 0</td>
+									<td>2, 3</td>
+								</tr>
+							</tbody>
+						</table>
 					</div>
-					<div className="col l2">
-						<button className='btn' type="submit" name="move" value="game" onClick={ this.nice.bind(this) } >Game</button>
+					<div className="row">
+						<div className="col l4"></div>
+						<div className="col l2">
+							<button className='btn' type="submit" name="move" value="opera">Opera</button>
+						</div>
+						<div className="col l2">
+							<button className='btn' type="submit" name="move" value="game" onClick={ this.nice.bind(this, 0) } >Game</button>
+						</div>
+						<div className="col l4"></div>
 					</div>
-					<div className="col l4"></div>
 				</div>
-			</div>
-		)
+			)		
+		} else {
+			return(
+				<h1>Done</h1>
+			)
+		}
+
 	}
 }
 
