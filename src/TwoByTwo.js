b233ae1b4bf8eb33828f6918fcae916c39679ab9
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2019-03-27 13:50:27 -0400

    reformatted the consent form

--- src/TwoByTwo.js
+++ src/TwoByTwo.js
@@ -246,24 +246,43 @@ class TwoByTwo extends Component {
 						<p>
 							Voluntary Consent: Dr. Nathan McNeese, and Lorenzo Barberis Canonico are inviting you to take part in a research study. Dr. McNeese is a professor at Clemson University. Lorenzo is a graduate student at Clemson University, running this study with the help of Dr. McNeese. 
 							You may choose not to take part and you may choose to stop taking part at any time. You will not be punished in any way if you decide not to be in the study or to stop taking part in the study. If you choose to stop taking part in this study, the information you have already provided will be used in a confidential manner.
+						</p>
+						<p>
 							Alternative to Participation: Participation is voluntary and the only alternative is to not participate.
+						</p>
+						<p>
 							Study Purpose: The purpose of this research is to evaluate Human-Artificial Intelligence (AI) teamwork.
+						</p>
+						<p>
 							Activities and Procedures: Your part in the study will be to play a series of games with both human and AI players, and will take a survey at the end. 
+						</p>	
+						<p>
 							Participation Time: It will take you around 20 minutes, at most 30, to be in this study.
+						</p>
+						<p>	
 							Risks and Discomforts: We do not know of any risks or discomforts to you in this research study.
+						</p>
+						<p>
 							Possible Benefits: The study will enable you to interact with a variety of different types of AIs over a series of game theory scenarios. You will get to see and observe how different AIs play the game differently, and which kind you collaborate most well with. It will be a fun way to learn about and contribute to cutting-edge research about the role of AI and cognitive technology to improve human-AI teamwork teamwork. The results will give us insight into the Human-AI relationship when it comes to cooperation, teamwork, and decision-making.
+						</p>
+						<p>
 							INCENTIVES
 							Upon completion of the experiment, you will be awarded $1 through Amazon’s Mechanical Turk. Partial payment will not be offered. 
+						</p>
+						<p>
 							PROTECTION OF PRIVACY AND CONFIDENTIALITY
 							No identifiable data will be recorded, and every data collected will be anonymized. The information collected during the study will not be used or distributed for future research studies.
 							The results of this study may be published in scientific journals, professional publications, or educational presentations. 
+						</p>
+						<p>
 							Contact Information
 							If you have any questions or concerns about your rights in this research study, please contact the Clemson University Office of Research Compliance (ORC) at irb@clemson.edu. The Clemson IRB will not be able to answer some study-specific questions. However, you may contact the Clemson IRB if the research staff cannot be reached or if you wish to speak with someone other than the research staff.
 							If you have any study related questions or if any problems arise, please contact Lorenzo at Clemson University at lorenzb@g.clemson.edu.
+						</p>
+						<h5>
 							Consent
 							By participating in the study, you indicate that you have read the information written above, are at least 18 years of age, been allowed to ask any questions, and are voluntarily choosing to take part in this research. You do not give up any legal rights by taking part in this research study.
-							A copy of this form will be given to you.
-						</p>
+						</h5>
 						<button className="btn" onClick={ this.forward.bind(this) }>Agree & Proceed</button>
 					</div>
 				)
