508f421f0fee276ae12249e21ddfa171ea792394
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2021-03-28 18:13:33 -0400

    Updates

---
+++ precache-manifest.b612b0429c86287f417e8d521a6e8eeb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15e72daa4d6e6d85f08",
+    "url": "/gt4teams_frontend/static/js/main.2b4a6d51.chunk.js"
+  },
+  {
+    "revision": "062643e1af8ecfc1e8fe",
+    "url": "/gt4teams_frontend/static/js/runtime~main.44582e2d.js"
+  },
+  {
+    "revision": "6e815e476beb63eca0f7",
+    "url": "/gt4teams_frontend/static/js/2.3c547ca1.chunk.js"
+  },
+  {
+    "revision": "de0fb09468226b01eb4925e81778cf2d",
+    "url": "/gt4teams_frontend/index.html"
+  }
+];
